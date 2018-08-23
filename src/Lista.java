c41e1344937393ea15a014ccac3d262854b0c920
João Bond <jobond46@gmail.com>
2018-08-22 20:41:12 -0400

    "2, 3, 4"

--- src/Lista.java
+++ src/Lista.java
@@ -1,5 +1,5 @@
-//Feitos:
-//Não feitos: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+//Feitos: 2, 3, 4
+//Não feitos: 1,4, 5, 6, 7, 8, 9, 10
 public class Lista {
 
     private No inicio;
@@ -100,7 +100,7 @@ public class Lista {
     //01
     public boolean removerExtremidades(){
         if(!isVazia()){
-            this.remover();
+            this.remover(1);
             this.remover(quantidade);
             return true;
         }else{
@@ -108,4 +108,179 @@ public class Lista {
             return false;
         }
     }
+
+    //02
+    public double maior(){
+        double maior = 0;
+        int contador = 0;
+        if(!isVazia()){
+            No aux = this.inicio;
+            while(aux != null) {
+                if(aux.getElemento()>maior){
+                    maior = aux.getElemento();
+                }
+                aux = aux.getProximo();
+            }
+            return maior;
+        }else{
+            System.out.println("Não foi possível mostrar o maior, pois a lista está vazia!");
+            return 0.0;
+        }
+    }
+    //02
+    public double menor() {
+        double menor = 0;
+        int contador = 0;
+        if (!isVazia()) {
+            No aux = this.inicio;
+            while (aux != null) {
+                if (contador == 0) {
+                    menor = aux.getElemento();
+                    contador++;
+                } else if (aux.getElemento() < menor) {
+                    menor = aux.getElemento();
+                }
+                aux = aux.getProximo();
+            }
+            return menor;
+        }else{
+            System.out.println("Não foi possível mostrar o menor, pois a lista está vazia!");
+            return 0.0;
+        }
+    }
+    //02
+    public double media() {
+        double media = 0;
+        int acumulador = 0;
+        double contador = 0;
+        if (!isVazia()) {
+            No aux = this.inicio;
+            while(aux != null) {
+                acumulador+=aux.getElemento();
+                aux = aux.getProximo();
+                contador++;
+            }
+            media = acumulador/contador;
+            return media;
+        }else{
+            System.out.println("Não foi possível calcular, pois a lista está vazia!");
+            return 0.0;
+        }
+    }
+
+    //03
+    public int pares() {
+        int contador = 0;
+        if (!isVazia()) {
+            No aux = this.inicio;
+            while(aux != null) {
+                if(aux.getElemento()%2 == 0){
+                    contador++;
+                }
+                aux = aux.getProximo();
+            }
+            return contador;
+        }else{
+            System.out.println("Não foi possível ver quantos pares tem, pois a lista está vazia!");
+            return 0;
+        }
+    }
+
+    //04
+    public Lista trocarValor(Lista lista, int antigo, int novo){
+        Lista nova = new Lista();
+        No aux = lista.inicio;
+        if(!lista.isVazia()){
+            while(aux != null){
+                if(aux.getElemento() == antigo){
+                    nova.inserirFim(new No(novo));
+                }else{
+                    nova.inserirFim(new No(aux.getElemento()));
+                }
+                aux = aux.getProximo();
+            }
+            return nova;
+        }else{
+            System.out.println("A lista passada está vazia!");
+            return null;
+        }
+    }
+
+    public boolean contem(int valor){
+        No aux = this.inicio;
+        if(!isVazia()){
+            while(aux != null){
+                if(aux.getElemento() == valor){
+                    return true;
+                }else{
+                    aux = aux.getProximo();
+                }
+            }
+            return false;
+        }else{
+            System.out.println("A lista está vazia!");
+            return false;
+        }
+    }
+
+    public Lista eliminarRepetidos(){
+        Lista nova = new Lista();
+        No aux = this.inicio;
+        if(!isVazia()){
+            while(aux != null){
+                if(!nova.contem(aux.getElemento())){
+                    nova.inserirFim(new No(aux.getElemento()));
+                }
+                aux = aux.getProximo();
+            }
+            return nova;
+        }else{
+            System.out.println("A lista está vazia, chefe!");
+            return null;
+        }
+    }
+
+    public Lista clonarNaLista(Lista parametro){
+        No aux = this.inicio;
+        if(!isVazia()){
+            while(aux != null){
+                parametro.inserirFim(new No(aux.getElemento()));
+                aux = aux.getProximo();
+            }
+            return parametro;
+        }else{
+            System.out.println("A lista está vazia!");
+            return null;
+        }
+    }
+
+    //05
+    public Lista uniao(Lista parametro){
+        if(!isVazia() && !parametro.isVazia()){
+            Lista todos = this.clonarNaLista(parametro);
+            todos = todos.eliminarRepetidos();
+            return todos;
+        }else{
+            System.out.println("Aconteceu algum erro!");
+            return null;
+        }
+
+    }
+
+    //05
+    public Lista interseccao(Lista parametro){
+        if(!isVazia() && !parametro.isVazia()){
+            Lista todos = this.clonarNaLista(parametro);
+            Lista nova = new Lista();
+            No aux = todos.inicio;
+            while(aux != null){
+
+            }
+            return todos;
+        }else{
+            System.out.println("Aconteceu algum erro!");
+            return null;
+        }
+    }
+
 }
--- src/TestarLista.java
+++ src/TestarLista.java
@@ -3,6 +3,10 @@ public class TestarLista {
     public static void main(String[] args) {
 
         Lista lista = new Lista();
+        Lista teste = new Lista();
+        teste.inserirFim(new No(0));
+        teste.inserirFim(new No(1));
+        teste.inserirFim(new No(8));
 
         lista.inserirInicio(new No(10));
         lista.inserirInicio(new No(12));
@@ -10,7 +14,37 @@ public class TestarLista {
         lista.inserirFim(new No(7));
         lista.inserirFim(new No(6));
         lista.inserir(new No(9), 3);
-
+        System.out.println("Lista: ");
         lista.imprimir();
+        System.out.println("------");
+        System.out.println("Elemento removido:" + lista.remover(3).getElemento());
+        System.out.println("Lista: ");
+        lista.imprimir();
+        System.out.println("------");
+        System.out.println("Extremidades Removidas!");
+        //lista.removerExtremidades();
+        System.out.println("Lista: ");
+        lista.imprimir();
+        System.out.println("------");
+        System.out.println("Menor: "+lista.menor());
+        System.out.println("Maior: "+lista.maior());
+        System.out.println("Média: "+lista.media());
+        System.out.println("Quantidade de pares: "+lista.pares());
+        //lista = lista.trocarValor(lista, 8,10);
+        System.out.println("------");
+        System.out.println("Lista: ");
+        lista.imprimir();
+        System.out.println("Contém 10? "+lista.contem(10));
+        lista = lista.eliminarRepetidos();
+        System.out.println("Sem repetidos: ");
+        System.out.println("------");
+        lista.imprimir();
+        System.out.println("Clonando na lista 'teste '");
+        lista.clonarNaLista(teste);
+        System.out.println("---TESTE---");
+        teste.imprimir();
+        System.out.println("--UNIAO----");
+        Lista uniao = lista.uniao(teste);
+        uniao.imprimir();
     }
 }
